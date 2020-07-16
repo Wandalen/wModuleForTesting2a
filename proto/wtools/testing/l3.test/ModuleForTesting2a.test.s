fd87908a46d630a5d8bcbd0129a3c5c074f628ab
wandalen <wandalen.me@gmail.com>
2020-07-16 16:16:26 +0300

    .

--- proto/wtools/testing/l3.test/ModuleForTesting2a.test.s
+++ proto/wtools/testing/l3.test/ModuleForTesting2a.test.s
@@ -5,7 +5,7 @@
 if( typeof module !== 'undefined' )
 {
 
-  var _ = require( '../l3/testing2a/Include.s' );
+  let _ = require( '../l3/testing2a/Include.s' );
 
   require( 'wTesting' );
 
@@ -36,7 +36,7 @@ function trivial( test )
 // declare
 // --
 
-var Self =
+let Self =
 {
 
   name : 'Tools.base.l3.ModuleForTesting2a',
--- sample/Sample.s
+++ sample/Sample.s
@@ -1,4 +1,4 @@
-var _ = require( '../' )
+let _ = require( '../' )
 
 var result = _.squareOfMul( 1, 2, 3 );
 
